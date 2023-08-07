130939733b1bef22c9fb3b8dd44fc58be027d38c
Jacques0813 <jacquesvdw2002@gmail.com>
2023-08-07 15:34:19 +0200

    Critical Analysis class added

---
+++ CriticalAnalysis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR381_Project
+{
+    internal class CriticalAnalysis
+    {
+        private double[,] initialTable;
+        private double[,] finalTable;
+
+        public CriticalAnalysis(double[,] initialTable, double[,] finalTable)
+        {
+            this.InitialTable = initialTable;
+            this.FinalTable = finalTable;
+        }
+
+        public double[,] InitialTable { get => initialTable; set => initialTable = value; }
+        public double[,] FinalTable { get => finalTable; set => finalTable = value; }
+    }
+}
