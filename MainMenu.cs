19fae540ea0481f20e9feadeef25278ffcd7717f
Jacques0813 <jacquesvdw2002@gmail.com>
2023-08-11 16:47:45 +0200

    Revert "BB algo and drawing of branches"
    This reverts commit 81dbc55f21d3f69ee6dbb3b5e17714f6773de5a3.

--- MainMenu.cs
+++ MainMenu.cs
@@ -44,54 +44,6 @@ namespace LPR381_Project
             cboCARangeCol.Enabled = false;
 
             cbForm.Location = new System.Drawing.Point(1164, 4);
-            //Testing here
-            List<BranchTable> branchTables = new List<BranchTable>
-            {
-                new BranchTable("1", new double[,] { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("11", new double[,] { { 11, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("12", new double[,] { { 12, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("111", new double[,] { { 111, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("112", new double[,] { { 112, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("121", new double[,] { { 121, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("122", new double[,] { { 122, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("2", new double[,] { { 2, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("21", new double[,] { { 21, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("22", new double[,] { { 22, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("211", new double[,] { { 211, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("212", new double[,] { { 212, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("221", new double[,] { { 221, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-                new BranchTable("222", new double[,] { { 222, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } }),
-            };
-            List<BranchTable> placementQueue = branchTables.OrderBy(x => x.Level.Length).ToList();
-
-            List<BranchTable> sub1 = branchTables.Where(x => x.Level[0] == '1').ToList();
-
-            List<BranchTable> sub2 = branchTables.Where(x => x.Level[0] == '2').ToList();
-
-
-            double x = Math.Pow(2, placementQueue.Last().Level.Length) / 2;
-            int y = placementQueue.Select(x => x.Level.Length).Distinct().Count();
-
-            int width = (int) x * placementQueue.Last().DataGrid.ClientSize.Width + 100;
-            int height = y * placementQueue.Last().DataGrid.ClientSize.Height + 100;
-
-            int tableWidth = placementQueue.Last().DataGrid.Width;
-
-            for (int i = 0; i < sub1.Count(); i++)
-            {
-                string level = sub1[i].Level;
-
-                sub1[i].DataGrid.Left = width / 2 - sub1[i].DataGrid.Width / 2;
-                sub1[i].DataGrid.Top += sub1.Last().DataGrid.Height + 50;
-
-                for (int j = 1; j < sub1[i].Level.Length; j++)
-                {
-                    sub1[i].DataGrid.Top += sub1.Last().DataGrid.Height + 50;
-                    sub1[i].DataGrid.Left += (sub1[i].Level[j] == '1' ? -1 : 1) * (pnlBranches.Width / (int)Math.Pow(2, j)) / 2;
-                }
-                pnlBranches.Controls.Add(sub1[i].DataGrid);
-                sub1[i].DataGrid.CurrentCell = null;
-            }
         }
 
         private void pnlDragnDrop_DragEnter(object sender, DragEventArgs e)
